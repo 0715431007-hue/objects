80f0b09e5380dc709d1c61c9b24d0414a83797b0
0715431007-hue <0715431007@s.tusd1.org>
2026-04-14 15:46:37 +0000

    commit

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
 console.log("Script started")
 
 // TODO: Test working with objects here
+let person = { name: "Lily", age: "15"};
+console.log(person);
+person.name = "lily";
+console.log(person.name);
 
 
-
-function processForm() {
+function processForm(event) {
     console.log("click");
     // TODO: prevent this function from reloading the page when the form is submitted
-
+    event.preventDefault();
     // TODO: Create a newUser object that has all the user's info from the form
-
+    let newUser = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        birthdate: document.getElementById("birthdate").value,
+        favColor: document.getElementById("favcolor").value
+    }
     // TODO: Call the addUser function and pass the newUser object as a parameter
+    
 }
 
 function addUser(user) {
     // Create a div to hold the user's info
     let div = document.createElement("div");
     // TODO: Set the background color of the div to the user's favorite color
+    div.style.backgroundColor = user.favColor;
 
     // Create a heading for the user's name
     let name = document.createElement("h2");
     // TODO: Set the text using the object data
+    name.innerText = user.name;
     div.appendChild(name); // Add the heading to the div
 
     // Create a paragraph for the user's email
